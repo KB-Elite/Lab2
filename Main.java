21800ee0f372352c94f7541b12ba49dfff436668
Andrey <andreusamadeus@yandex.ru>
2015-09-28 20:27:43 +0300

    First commit with initialize main menu.

---
+++ Main.java
@@ -0,0 +1,30 @@
+//это основной класс для лабы в нем будет меню для запуска соответствующего задания
+//решение заданий должно быть каждый в своем классе с именем "TaskX", где Х - это номер задания
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int choise;
+		do{
+			System.out.println();
+			System.out.println("\tMENU:");
+			System.out.println("-1. Пример");
+                        System.out.println("0. Завершить работу");
+			System.out.println("1. Задание 1");
+
+			choise = scan.nextInt();
+			switch (choise){
+				case -1:new Primer();
+					break;
+                                case 0: System.exit(0);
+                                        break;
+                                case 1:
+                                        break;
+				default:
+					System.out.println("Invalid menu number! Try again!");
+			}
+		}
+		while (true);
+	}
+}
